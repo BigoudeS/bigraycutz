f422570ee2c940776c4cc49c8ce217dbd1c47536
Stephen <stephen.bigoude@fujitsu.com>
2021-04-17 18:44:45 +0100

    price update

--- src/grtrmcr.js
+++ src/grtrmcr.js
@@ -9,25 +9,33 @@ function grtrmcr() {
       <div className="App">
           <Card className="text-center">
           <Card.Header>These are areas outside of Manchester, Salford & Tameside but within Greater Manchester like Bolton, Bury, Stockport, etc.</Card.Header>
+          <Card.Header style={{fontSize:"small"}}>* Early & Late Bookings will require a £10 deposit. If you give 6 hours notice before you're appointment for cancellation, your deposit will be refunded. Fail to give notice before 6 hours beforehand, deposit will not be refunded</Card.Header>
+          
   <Card.Body>
     <Card.Title><h1>Greater Manchester</h1></Card.Title>
     <Card.Text>
     <hr></hr>
-        <h3>Standard Booking</h3>
-    Hours from 11am-8pm <br></br>
+<h3>Standard Booking</h3>
+Hours from 11am-8pm <br></br>
 Haircuit & Beard Shape Up - £25 <br></br>
+Shape up & Beard Shape Up - £20 <br></br>
+Kids from the age 16 and under - £20 <br></br>
 
 <hr></hr>
 
-<h3>Early Booking</h3>
+<h3>*Early Booking</h3>
 bookings from 6am-11am<br></br>
-Haircut & Beard Shape Up - £30 <br></br>
+Haircut & Beard Shape Up - £35 <br></br>
+Shape up & Beard Shape Up - £30 <br></br>
+Kids from the age 16 and under - £30 <br></br>
 
 <hr></hr>
 
-<h3>Late Booking</h3>
+<h3>*Late Booking</h3>
 bookings from 8pm-12am<br></br>
 Haircut & beard Shape Up - £40<br></br>
+Shape up & Beard Shape Up - £35 <br></br>
+Kids from the age 16 and under - £35 <br></br>
 
     </Card.Text>
 
--- src/mcr.js
+++ src/mcr.js
@@ -9,6 +9,7 @@ function mcr() {
       <div className="App">
           <Card className="text-center">
   <Card.Header>All these prices are subject to areas in Manchester, Salford & Tameside as they are in close proximity to where I live.</Card.Header>
+  <Card.Header style={{fontSize:"small"}}>* Early & Late Bookings will require a £10 deposit. If you give 6 hours notice before you're appointment for cancellation, your deposit will be refunded. Fail to give notice before 6 hours beforehand, deposit will not be refunded</Card.Header>
   <Card.Body>
     <Card.Title><h1>Manchester & Salford</h1></Card.Title>
     <Card.Text>
@@ -21,17 +22,19 @@ Kids from the age 16 and under - £10 <br></br>
 
 <hr></hr>
 
-<h3>Early Booking</h3>
+<h3>*Early Booking</h3>
 bookings from 6am-11am<br></br>
 Haircut & Beard Shape Up - £25 <br></br>
 Shape up & Beard Shape - £20 <br></br>
+Kids from the age 16 and under - £20 <br></br>
 
 <hr></hr>
 
-<h3>Late Booking</h3>
+<h3>*Late Booking</h3>
 bookings from 8pm-12am<br></br>
-Haircut & beard Shape Up - £35<br></br>
-Shape up & Beard Shape Up - £30<br></br>
+Haircut & beard Shape Up - £30<br></br>
+Shape up & Beard Shape Up - £25<br></br>
+Kids from the age 16 and under - £25 <br></br>
 
     </Card.Text>
 
