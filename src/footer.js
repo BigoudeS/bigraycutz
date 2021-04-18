7b3e8048203fdc73c10b490b112a938a09d2b908
Stephen <stephen.bigoude@fujitsu.com>
2021-04-18 12:47:22 +0100

    socials update

--- src/footer.js
+++ src/footer.js
@@ -3,6 +3,8 @@ import './App.css';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import InstagramIcon from '@material-ui/icons/Instagram';
+import WhatsappIcon from '@material-ui/icons/WhatsApp';
+import YoutubeIcon from '@material-ui/icons/YouTube';
 
 function Footer() {
 
@@ -13,9 +15,11 @@ function Footer() {
             <a href="https://www.facebook.com/pages/category/Barber-Shop/Big-Ray-Cutz-103155061092391/" class="socials" style={{paddingRight:3,paddingLeft:3}}><FacebookIcon style={{fontSize:40}}/></a>
             <a href="https://twitter.com/bigraycutz" class="socials" style={{paddingRight:3,paddingLeft:3}}><TwitterIcon style={{fontSize:40}}/></a>
             <a href="https://instagram.com/bigraycutz?igshid=j6uamfakd39q" class="socials" style={{paddingRight:3,paddingLeft:2}}><InstagramIcon style={{fontSize:39}}/></a>
+            <a href="https://wa.me/447990394004" class="socials" style={{paddingRight:3,paddingLeft:2}}><WhatsappIcon style={{fontSize:39}}/></a>
+            <a href="https://www.youtube.com/channel/UC_koENUjpy6ytX5eTIyLvIg/featured" class="socials" style={{paddingRight:3,paddingLeft:2}}><YoutubeIcon style={{fontSize:42}}/></a>
 
             <br></br>
-            <sub><i>Big Ray Cutz© 2020</i></sub>
+            <sub><i>Big Ray Cutz© 2021</i></sub>
         </div>
 
     );}
